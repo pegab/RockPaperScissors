dd0af40cf194fc5f21a43d011e57fa3aaafd247e
pegab <p.gabriel@posteo.de>
2026-01-03 10:28:32 +0100

    update logic -> integrated switch algo, UI

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -15,9 +15,12 @@ struct ContentView: View {
     @State private var startValue = Int.random(in: 0...2)
     @State private var winOrLoose = Bool.random()
     @State private var score = 0
+    
     @State private var endOfGame = false
     @State private var endOfGameTitle = "END OF GAME"
     @State private var maxQuestions: Int = 0
+    
+    //@State private var userChoise = 0
     
     
     
@@ -32,48 +35,57 @@ struct ContentView: View {
                         
                         Spacer()
                         
-                        Text("Score: \(score)")
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .foregroundStyle(.white)
-                        
-                        Spacer()
-                        
-                        if values[startValue] == "Rock" {
-                            Text("🪨")
-                                .font(.system(size: 150))
+                        VStack{
+                            if winOrLoose {
+                                Text("Which one wins?")
+                                    .padding()
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                                    .foregroundStyle(.green)
+                                
+                            } else {
+                                Text("Which one loses?")
+                                    .padding()
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                                    .foregroundStyle(.red)
+                                
+                            }
                             
-                        } else if values[startValue] == "Paper" {
-                            Text("📄")
-                                .font(.system(size: 150))
-                        } else {
-                            Text("✂️")
-                                .font(.system(size: 150))
                         }
                         
                         Spacer()
                         
-                        if winOrLoose {
-                            Text("Which one wins?")
-                                .padding()
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .foregroundStyle(.green)
+                        VStack{
+                            if values[startValue] == "Rock" {
+                                Text("🪨")
+                                    .font(.system(size: 150))
                                 
-                        } else {
-                            Text("Which one loses?")
-                                .padding()
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .foregroundStyle(.red)
-                                
+                            } else if values[startValue] == "Paper" {
+                                Text("📄")
+                                    .font(.system(size: 150))
+                            } else {
+                                Text("✂️")
+                                    .font(.system(size: 150))
+                            }
                         }
                         
+                        Spacer()
                         
+                        VStack{
+                            Spacer()
+                            
+                            Text("Score: \(score)")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundStyle(.white)
+                        }
+                       
                         
                         HStack (spacing: 20){
-                            //winorloose bool entscheidet welche func vom Button ausgeführt wird
-                            Button(action: winOrLoose ? rocking : notRocking) {
+                          
+                            Button{ winLoseCalculator(userChoise: 0) }
+                            label: {
                                 Text("🪨")
                                     .font(.largeTitle)
                                     .padding()
@@ -81,7 +93,8 @@ struct ContentView: View {
                                     .clipShape(.circle)
                             }
                             
-                            Button(action: winOrLoose ? papering : notPapering) {
+                            Button{ winLoseCalculator(userChoise: 1) }
+                            label: {
                                 Text("📄")
                                     .font(.largeTitle)
                                     .padding()
@@ -89,7 +102,8 @@ struct ContentView: View {
                                     .clipShape(.circle)
                             }
                             
-                            Button(action: winOrLoose ? scissoring : notScissoring) {
+                            Button{ winLoseCalculator(userChoise: 2) }
+                            label: {
                                 Text("✂️")
                                     .font(.largeTitle)
                                     .padding()
@@ -120,128 +134,46 @@ struct ContentView: View {
     
     /*die funcs funktionieren wie folgt:
      
-     es wird überprüft welcher startvalue angezeigt wird (der startvalue gibt an welches symbol der computer ausgewählt hat)
-     0 = stein, 1 = papier, 2 = schere
-     
-     rocking/not rocking werden getriggert wenn man den rock button drückt, usw.
-     
-     also wenn der computer die schere auswählt und die frage which one wins, dann nimmt der user um zu gewinnen den rock button -> dieser triggert die func rocking (da gefragt wurde which one wins?)
-     rocking übeprüft nun ob der user richtig liegt, der rock gewinnt wenn der computer 2 also die schere gewählt hat, da dem so war bekommt der user einen punkt und der startValue wird wieder randomisiert, dh der computer wählt ein neues Symbol und die Frage win or lose wird getoggelt.
+     die Userchoise wird hardcoded vom Button Press übermittelt
+     switch vergleicht startvalue = cpuChoise mit unserChoise
+     if winOrLoose == true dann werden die cases angeführt in denen der User
+     gewinnt, zb: 0,1 CPU hat Rock User hat Papier somit gewinnt der User,
+     usw.
+     danach bekommt er einen Punkt, in allen anderen Fällen (default) verliert der User und ein Punkt wird abgezogen
+     danach wird das Spiel für eine neue Runde zurückgesetzt und die Anzahl der maxFragen um 1 erhöht (nach 5 Fragen ist das Spiel beendet)
+     sollte winOrLoose false sein wird der else Block ausgeführt der genau
+     gegensätzlich funktioniert.
+    
     */
-    func rocking() {
-        
-        if startValue == 2 {
-            score += 1
+    
+    func winLoseCalculator(userChoise: Int) {
+        if winOrLoose == true {
+            switch (startValue, userChoise){
+            case (0,1), (1,2), (2,0): score += 1
+            default: score -= 1
+            }
             startValue = Int.random(in: 0...2)
             winOrLoose.toggle()
+            maxQuestions += 1
+            
+            if maxQuestions == 5 {
+                endOfGame = true
+            }
         } else {
-            score -= 1
+            switch (startValue, userChoise) {
+            case (0,2), (1,0), (2,1): score += 1
+            default: score -= 1
+            }
             startValue = Int.random(in: 0...2)
             winOrLoose.toggle()
-        }
-        
-        maxQuestions += 1
-        
-        if maxQuestions == 5 {
-            endOfGame = true
+            maxQuestions += 1
+            
+            if maxQuestions == 5 {
+                endOfGame = true
+            }
         }
     }
-    
-    func notRocking() {
-      if startValue == 1 {
-            score += 1
-            startValue = Int.random(in: 0...2)
-            winOrLoose.toggle()
-        } else {
-            score -= 1
-            startValue = Int.random(in: 0...2)
-            winOrLoose.toggle()
-        }
-        
-        maxQuestions += 1
-        
-        if maxQuestions == 5 {
-            endOfGame = true
-        }
-
-    }
-    
-    func papering() {
-       if startValue == 0 {
-            score += 1
-            startValue = Int.random(in: 0...2)
-            winOrLoose.toggle()
-        } else {
-            score -= 1
-            startValue = Int.random(in: 0...2)
-            winOrLoose.toggle()
-        }
-        
-        maxQuestions += 1
-        
-        if maxQuestions == 5 {
-            endOfGame = true
-        }
-
-    }
-    
-    func notPapering() {
-        if startValue == 2 {
-            score += 1
-            startValue = Int.random(in: 0...2)
-            winOrLoose.toggle()
-        } else {
-            score -= 1
-            startValue = Int.random(in: 0...2)
-            winOrLoose.toggle()
-        }
-        
-        maxQuestions += 1
-        
-        if maxQuestions == 5 {
-            endOfGame = true
-        }
-
-    }
-    
-    
-    func scissoring() {
-        if startValue == 1 {
-            score += 1
-            startValue = Int.random(in: 0...2)
-            winOrLoose.toggle()
-        } else {
-            score -= 1
-            startValue = Int.random(in: 0...2)
-            winOrLoose.toggle()
-        }
-        
-        maxQuestions += 1
-        
-        if maxQuestions == 5 {
-            endOfGame = true
-        }
-
-    }
-    
-    func notScissoring() {
-        if startValue == 0 {
-            score += 1
-            startValue = Int.random(in: 0...2)
-            winOrLoose.toggle()
-        } else {
-            score -= 1
-            startValue = Int.random(in: 0...2)
-            winOrLoose.toggle()
-        }
-        maxQuestions += 1
-        
-        if maxQuestions == 5 {
-            endOfGame = true
-        }
-
-    }
-    
+    //wird via .alert ausgeführt
     func restartGame() {
         score = 0
         maxQuestions = 0
